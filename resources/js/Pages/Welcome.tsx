8000cb97a56188ce88651ab99cb0873bb06f442a
Glenn Raya <rayaglenn@gmail.com>
2024-09-07 21:33:39 +0800

    Change page title

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -17,7 +17,7 @@ export default function Welcome({
 
     return (
         <>
-            <Head title="Welcome - GitHub Actions - Push to Deploy" />
+            <Head title="Welcome - VPS Provisioning for Laravel" />
             <div className="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
                 <img
                     id="background"
